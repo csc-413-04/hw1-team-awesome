264657496e6c7a56696a2c478f2723571e652ffa
Abdulaziz Alharbi <abdulazizalharbi@sfs-wifi-dhcp-10-143-248-106.wifi.sfsu.edu>
2018-10-04 19:07:20 -0700

    Users calss is created with HashMap

---
+++ src/simpleserver/Users.java
@@ -0,0 +1,11 @@
+package simplserver;
+import java.util.*;
+
+public class Users{
+
+private final static Map<Integer, Users> user_all = new HashMap<>();
+private String username;
+private int userid;
+
+
+}
