3b119040b0095799f42438dca9019856cebbac85
Abdulaziz Alharbi <abdulazizalharbi@sfs-wifi-dhcp-10-143-248-106.wifi.sfsu.edu>
2018-10-04 19:23:13 -0700

    adding hashMap, postid, userid, and data

--- src/simpleserver/Posts.java
+++ src/simpleserver/Posts.java
@@ -1,4 +1,12 @@
 package simpleserver;
 
+import java.util.*;
+
 public class Posts {
+
+ private final static Map<Integer, Posts> posts_all = new HashMap<>();
+    private int postid;
+    private int userid;
+    private String data;
+
 }
