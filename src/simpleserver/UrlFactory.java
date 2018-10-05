bc04247f1c6b7e5e7ea69fa2386a84e8a0bd6154
Abdulaziz Alharbi <abdulazizalharbi@sfs-wifi-dhcp-10-143-248-106.wifi.sfsu.edu>
2018-10-04 19:36:12 -0700

    adding adding user_All, Poss_All, ArrayList for User&post

--- src/simpleserver/UrlFactory.java
+++ src/simpleserver/UrlFactory.java
@@ -1,4 +1,18 @@
 package simpleserver;
 
+import com.google.gson.*;
+import java.util.ArrayList;
+
 public class UrlFactory {
+
+Users[] user_All = Parser.getUserObject();
+    Posts[] posts_All = Parser.getPostObjects();
+    ArrayList<Users> users_Array = new ArrayList<>();
+    ArrayList<Posts> posts_Array = new ArrayList<>();
+    String response = "<code><p style=\"position: relative; left:50px\">\"status\":\"SUCCESS\"</p><p style=\"position: relative; left:50px\">\"entries\":";
+    String fail = "<code><p style=\"position: relative; left:50px\">\"status\":\"ERROR\"</p></code>";
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+
+
 }
